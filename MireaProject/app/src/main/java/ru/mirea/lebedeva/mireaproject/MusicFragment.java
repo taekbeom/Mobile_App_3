9098aafaae5614cb13913ef07b0189675b2af891
taekbeom <oumamarrysaihara@gmail.com>
2022-03-28 18:17:20 +0300

    Дополнение к 4 практике

---
+++ MireaProject/app/src/main/java/ru/mirea/lebedeva/mireaproject/MusicFragment.java
@@ -0,0 +1,95 @@
+package ru.mirea.lebedeva.mireaproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MusicFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MusicFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public MusicFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment MusicFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MusicFragment newInstance(String param1, String param2) {
+        MusicFragment fragment = new MusicFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    private Button buttonStart;
+    private Button buttonStop;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View rootView = inflater.inflate(R.layout.fragment_music, container, false);
+
+        buttonStart = rootView.findViewById(R.id.button10);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickPlayMusic(rootView);
+            }
+        });
+
+        buttonStop = rootView.findViewById(R.id.button11);
+        buttonStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickStopMusic(rootView);
+            }
+        });
+
+        return rootView;
+
+    }
+    public void onClickPlayMusic(View view) {
+        getActivity().startService(new Intent(getActivity(), PlayerService.class));
+    }
+    public void onClickStopMusic(View view) {
+        getActivity().stopService(new Intent(getActivity(), PlayerService.class));
+    }
+}
